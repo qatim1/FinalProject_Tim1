8e02da569fd6eef4209fd139bf88406427eb7da1
dendyindriyo <dendyindriyo@gmail.com>
2023-06-11 20:41:38 +0700

    Tambah folder "Dendy Indriyo Saputro" di test case & obj. repo.

---
+++ Object Repository/Dendy Indriyo Saputro/Mobile/asdadssad.rs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>asdadssad</name>
+   <tag></tag>
+   <elementGuidId>00000000-0000-0000-0000-000000000000</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
---
+++ Object Repository/Dendy Indriyo Saputro/WEB/fsafafa.rs
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>fsafafa</name>
+   <tag></tag>
+   <elementGuidId>f56db39c-6ac4-4103-b1de-2af33d48a52f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
