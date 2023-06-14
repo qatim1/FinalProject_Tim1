d2444defc22ecf30b36147437bf7ffbd2102f35f
adria <adria@DESKTOP-2B5N6V0>
2023-06-14 16:44:16 +0700

    Tambah Test Suite Web TC Negatif Register

---
+++ Test Suites/Adrian/WEB/N_Register.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>N_Register</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>14593837-c560-4d71-a5d9-93da0f877178</testSuiteGuid>
+   <testCaseLink>
+      <guid>6c2cdc44-29aa-4a5b-a07a-cce2d6228f2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Adrian Ramdo Firmansyah/Negative Test Case/WEB/WEBTCR_N_005_Registrasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a550129-3c31-4bb1-b95c-2d92f2dd55b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Adrian Ramdo Firmansyah/Negative Test Case/WEB/WEBTCR_N_006_Registrasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e662aeff-f625-41db-bf13-b5d8fe2710cb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Adrian Ramdo Firmansyah/Negative Test Case/WEB/WEBTCR_N_008_Registrasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>be072cae-b2d3-471e-b8d5-340b8a7cc0a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Adrian Ramdo Firmansyah/Negative Test Case/WEB/WEBTCR_N_011_Registrasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d8147028-fa90-4dce-bdb3-3e2da5a2028a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Adrian Ramdo Firmansyah/Negative Test Case/WEB/WEBTCR_N_016_Registrasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
