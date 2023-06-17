d2e0b7fa0c5ea2d59f58672863f413d2e83ff6fe
dendyindriyo <dendyindriyo@gmail.com>
2023-06-17 07:25:06 +0700

    Tambah 2 test suites dan edit 1 test suites

---
+++ Test Suites/Dendy Indriyo Saputro/WEB/WEBTS_001_Registrasi.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WEBTS_001_Registrasi</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>1cbdafde-0fca-43eb-a615-268ba8500801</testSuiteGuid>
+   <testCaseLink>
+      <guid>76fba286-e00b-48e7-806d-2717424118ff</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Dendy Indriyo Saputro/Negative Test Case/WEB/WEBTCR_N_001_Registrasi</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
